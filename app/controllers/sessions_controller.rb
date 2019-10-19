5d75f6514028b0b3d81c2883973ca894c1fe2ee5
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-10-19 14:16:18 +0900

    userのlogin処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
+  protect_from_forgery :except => [:session]
 
   def user_new
+    @user = User.new
   end
 
   def user_create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.password == params[:session][:password]
+      sign_in_user user
+      redirect_to root_path
+    else
+      render "user_new"
+    end
+
   end
 
   def user_destroy
