7147574b305458742fcb7d2cfe6392425e63cef7
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-10-19 09:59:57 +0900

    Userの新規登録、登録後のログイン処理を追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 module SessionsHelper
+  def sign_in_user(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def sign_in_user?
+    !current_user.nil?
+  end
 end
