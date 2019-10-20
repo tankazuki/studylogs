6144d1c0a0dba37baa51b056dad7069b0a05428c
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-10-20 14:41:41 +0900

    user modelのvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGAX}
+  validates :password_digest, presence: true, length: { minimum: 20 }
 end
