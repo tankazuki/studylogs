3b5423a364dcbf8662b762f9721c64dd5b882853
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-11-02 22:36:46 +0900

    トップページの処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "home#index"
+root "home#top"
 
 get "/users/new", to: "users#new", as: "user_new"
 post "/users/create", to: "users#create", as: "user_create"
